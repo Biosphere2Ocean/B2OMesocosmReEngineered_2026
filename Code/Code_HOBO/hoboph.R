ff7db67f35bc6c20ae8a81e1ed597e9272784a48
rgrambihler <rgrambihler@email.arizona.edu>
2023-10-03 18:18:52 -0700

    create descriptive stats script and edit hoboph.r file

--- Code/Code_HOBO/hoboph.R
+++ Code/Code_HOBO/hoboph.R
@@ -10,27 +10,28 @@ library(readxl) #including readxl package to read some of the folder files in ex
 #note: I prefer the package tsibble which includes some new features. tibbletime is actually retired. but tsibble was having trouble parsing the time column for some reason. it is more tempermental. there is also a timetk package I haven't used as much
 
 #import hobo ph data
-setwd("C:/Users/fried/OneDrive/Desktop/HOBO pH Data") #your data directory. might work with the "Box Sync" client as well, haven't tried
-csv <- list.files(pattern = ".csv") #create list of csvs in directory
-xls<-list.files(pattern = ".xls") #create list of excel files in director
+setwd("Data/RawData/RawData_HOBO/pH/") #your data directory. might work with the "Box Sync" client as well, haven't tried
+path = "Data/RawData/RawData_HOBO/pH/"
+csv <- list.files(path, pattern = ".csv") #create list of csvs in directory
+#xls<-list.files(path, pattern = ".xls") #create list of excel files in director
 
 #bind csv files into list. skips the first 2 lines. creates a column noting the source file of each entry
 hobocsvlist<-Map(cbind, lapply(csv, fread,sep=",",skip=2), filename = csv)
 
 #bind xls files into list. skips the first line. creates a column noting the source file of each entry
-hoboxlslist<-Map(cbind, lapply(xls, read_excel,skip=1), filename = xls)
+#hoboxlslist<-Map(cbind, lapply(xls, read_excel,skip=1), filename = xls)
 
 hobocsv<-rbindlist(hobocsvlist,use.names=TRUE,fill=TRUE) #bind csv list entries into single merged data frame
 hobocsv<-hobocsv[,c(1:4,15)]#exclude some of the calibration related data
 colnames(hobocsv)<-c("Datetime","TempF","mV","pH","filename") #rename columns
 hobocsv$Datetime<-as_datetime(hobocsv$Datetime) #change col1 format to date
 
-hoboxls<-rbindlist(hoboxlslist,use.names=TRUE,fill=TRUE) #bind xls list into single merged data frame
-hoboxls<-hoboxls[,c(2:6)] #remove col1
-colnames(hoboxls)<-c("Datetime","TempF","mV","pH","filename") #rename columns
-hoboxls$Datetime<-as_datetime(hoboxls$Datetime)#change col1 format to date
+#hoboxls<-rbindlist(hoboxlslist,use.names=TRUE,fill=TRUE) #bind xls list into single merged data frame
+#hoboxls<-hoboxls[,c(2:6)] #remove col1
+#colnames(hoboxls)<-c("Datetime","TempF","mV","pH","filename") #rename columns
+#hoboxls$Datetime<-as_datetime(hoboxls$Datetime)#change col1 format to date
 
-hoboph<-rbind(hobocsv,hoboxls) #bind the two data frames
+hoboph<- hobocsv #rbind(hobocsv,hoboxls) #bind the two data frames
 hoboph$Date<-as_date(hoboph$Datetime) #create a date-only column
 hoboph<- distinct(hoboph)#remove any duplicate rows
 hoboph<-subset(hoboph,is.na(hoboph$Datetime)==FALSE) #remove any NAs

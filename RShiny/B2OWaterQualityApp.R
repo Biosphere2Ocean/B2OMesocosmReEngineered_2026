2344234f4fb4a6bfbfa5d8825f04163ec6c3c09f
rgrambihler <rgrambihler@email.arizona.edu>
2024-08-20 16:44:18 -0700

    fixed HachYSI data files, worked on Overall Trends tab of RShiny app

--- RShiny/B2OWaterQualityApp.R
+++ RShiny/B2OWaterQualityApp.R
@@ -18,16 +18,21 @@ library(lubridate)
 # all data are medians of each indicated interval except for the daily hach 
 #  which are "raw" data points since hach is taken once per day (Mon & Fri)
 # daily Hach and YSI data
-dfDaily <- read_csv("SourceData/01-HachYSI-Data-Daily.csv", col_names = TRUE)
+dfDaily <- read_csv("SourceData/01-HachYSI-Data-Daily.csv", col_names = TRUE) %>%
+  rename(pH = `pH (YSI)`)
 # weekly Hach and YSI data 
-dfWeekly <- read_csv("SourceData/02-HachYSI-Data-Weekly.csv", col_names = TRUE)
+dfWeekly <- read_csv("SourceData/02-HachYSI-Data-Weekly.csv", col_names = TRUE) %>%
+  rename(pH = `pH (YSI)`)
 # monthly Hach and YSI data 
-dfMonthly <- read_csv("SourceData/03-HachYSI-Data-Monthly.csv", col_names = TRUE)
+dfMonthly <- read_csv("SourceData/03-HachYSI-Data-Monthly.csv", col_names = TRUE) %>%
+  rename(pH = `pH (YSI)`)
 # yearly Hach and YSI data 
-dfYearly <- read_csv("SourceData/04-HachYSI-Data-Yearly.csv", col_names = TRUE)
+dfYearly <- read_csv("SourceData/04-HachYSI-Data-Yearly.csv", col_names = TRUE) %>%
+  rename(pH = `pH (YSI)`)
 # all Hach and YSI data: combo of all above files with "Time Interval" column to 
 # denote which interval (daily, weekly, etc) the data comes from
-#dfAll <- read_csv("SourceData/05-HachYSI-Data-All.csv", col_names = TRUE)
+dfAll <- read_csv("SourceData/05-HachYSI-Data-All.csv", col_names = TRUE) %>%
+  rename(pH = `pH (YSI)`)
 
 # Define variables -----
 
@@ -91,9 +96,14 @@ ui <- fluidPage(
              # Overall Trends Page
              tabPanel("Overall Trends",
                       sidebarLayout(
+                        position = "left",
+                        fluid = TRUE,
                         # column: fixed width, date range input and inputs for 4 plots
                         sidebarPanel(
                           # input date range
+                          
+                          # CHECK BOX GROUP MIGHT FIX UI ISSUE
+                          
                           dateRangeInput(inputId = "OverallDateRange", 
                                          label = "Select Date Range",
                                          start = date_range$Date[1],
@@ -154,21 +164,101 @@ ui <- fluidPage(
                                         value = FALSE)
                         ),
                         mainPanel(
-                          # column that will fill remainder of page
-                          fillCol(
-                            fillRow(conditionalPanel("input.OverallVariable1 != None",
-                                                     plotOutput("OverallTrendsPlot1"),
-                                                     textOutput("testText"))),
-                            fillRow(conditionalPanel("input.OverallVariable2 != None",
-                                                     plotOutput("OverallTrendsPlot2")))
-                          ),
-                          # column that will pop up on end of previous column 
-                          fillCol(
-                            fillRow(conditionalPanel("input.OverallVariable3 != None",
-                                                     plotOutput("OverallTrendsPlot3"))),
-                            fillRow(conditionalPanel("input.OverallVariable4 != None",
-                                                     plotOutput("OverallTrendsPlot4")))
+                          #works
+                          # column(6,
+                          #        conditionalPanel("input.OverallVariable1 != None",
+                          #                         plotOutput("OverallTrendsPlot1", height = "600px")),
+                          #        conditionalPanel("input.OverallVariable2 != None",
+                          #                         plotOutput("OverallTrendsPlot2", height = "600px"))
+                          # ),
+                          # column(6,
+                          #        conditionalPanel("input.OverallVariable3 != None",
+                          #                         plotOutput("OverallTrendsPlot3", height = "600px")),
+                          #        conditionalPanel("input.OverallVariable4 != None",
+                          #                         plotOutput("OverallTrendsPlot4", height = "600px"))
+                          # )
+                          
+                          #works
+                          # fluidRow(
+                          #   conditionalPanel("input.OverallVariable1 != None",
+                          #                    plotOutput("OverallTrendsPlot1")),
+                          #   conditionalPanel("input.OverallVariable2 != None",
+                          #                    plotOutput("OverallTrendsPlot2"))
+                          # )
+                          
+                          fluidRow(
+                            conditionalPanel(
+                              "input.OverallVariable1 != None",
+                              plotOutput("OverallTrendsPlot1")
+                            ),
+                            conditionalPanel(
+                              "input.OverallVariable2 != None",
+                              plotOutput("OverallTrendsPlot2")
+                            )
                           )
+                        
+                        
+                          # fluidRow(
+                          #   plotOutput("OverallTrendsPlot1"),
+                          #   plotOutput("OverallTrendsPlot2"),
+                          #   plotOutput("OverallTrendsPlot3"),
+                          #   plotOutput("OverallTrendsPlot4")
+                          # )
+                          # conditionalPanel(condition = "input.OverallVariable3 == 'None'",
+                          #                  fluidRow(
+                          #                    plotOutput("OverallTrendsPlot1"),
+                          #                    plotOutput("OverallTrendsPlot2")
+                          #                  )
+                          # ),
+                          # conditionalPanel(condition = "input.OverallVariable3 != 'None'",
+                          #                  fluidRow(
+                          #                    column(6,
+                          #                           plotOutput("OverallTrendsPlot1")),
+                          #                    column(6,
+                          #                           plotOutput("OverallTrendsPlot3"))
+                          #                  ),
+                          #                  fluidRow(
+                          #                    column(6,
+                          #                           plotOutput("OverallTrendsPlot2")),
+                          #                    column(6,
+                          #                           plotOutput("OverallTrendsPlot4"))
+                          #                  )
+                          #                  
+                          # )
+                          
+                          # fluidRow(
+                          #     plotOutput("OverallTrendsPlot1"),
+                          #     plotOutput("OverallTrendsPlot2"),
+                          #   conditionalPanel(
+                          #     condition = "input.OverallVariable3 =! None",
+                          #     fluidRow(
+                          #       column(6,
+                          #              plotOutput("OverallTrendsPlot1")),
+                          #       column(6,
+                          #              plotOutput("OverallTrendsPlot3"))
+                          #     ),
+                          #     fluidRow(
+                          #       column(6,
+                          #              plotOutput("OverallTrendsPlot2")),
+                          #       column(6,
+                          #              plotOutput("OverallTrendsPlot4"))
+                          #     )
+                          #   )
+                          # )
+                          
+                          # column(5,
+                          #        conditionalPanel("input.variable1 != None",
+                          #                         plotOutput("plot1", height = "600px")),
+                          #        conditionalPanel("input.variable2 != None",
+                          #                         plotOutput("plot2", height = "600px"))
+                          # ),
+                          # column(5,
+                          #        conditionalPanel("input.variable3 != None",
+                          #                         plotOutput("plot3", height = "600px")),
+                          #        conditionalPanel("input.variable4 != None",
+                          #                         plotOutput("plot4", height = "600px"))
+                          # )
+                          
                         )
                       )
              ),
@@ -215,63 +305,49 @@ server <- function(input, output) {
   # Overall Trends
   ## Plot 1
   output$OverallTrendsPlot1 <- renderPlot({
+    # line graph of input variable
     
-    # # check which dataset is being viewed
-    # if (input$dataset3 == "Raw Data") {
-    #   # use raw data csv
-    #   data <- dfYSI
-    #   subt <- "Pre-QC: All Outliers Still Present"
-    # } else if (input$dataset3 == "Clean Data") {
-    #   # use qc'd data csv
-    #   data <- dfYSI
-    #   subt <- "Post-QC: Data Outside 2-Sigma from Mean has been Removed"
-    # }
-    # 
-    # if (input$variable3 == "None") {
-    #   x <- 1
-    # } else {
-    #   colnames(data)[3:19] <- var_choices[2:18]
-    #   data <- data[, c("date", input$variable3)]
-    #   colnames(data) <- c("date", "input_var")
-      
-      # line graph of input variable
+    dfAll_otp1 <- dfBaseData[, c("Time Interval", "Date", input$OverallVariable1)]
+    colnames(dfAll_otp1) <- c("Time Interval", "Date", "input_var")
     
-      # dfAll_otp1 <- dfAll[, c("Time Interval", "Date", input$OverallVariable1)]
-      # colnames(dfAll_otp1) <- c("Time Interval", "Date", "input_var")
-      
-      dfAll %>%
-        filter(Date >= input$OveralldateRange[1] & Date <= input$OveralldateRange[2]) %>%
-        filter(`Time Interval` == input$OverallMedian1) %>%
-        mutate(Date = as.Date(Date)) %>%
-        ggplot(aes(x = Date, 
-                   y = input$OverallVariable1))+
-        geom_line()+
-        # geom_smooth() +
-        # scale_x_date(date_breaks = "1 year",
-        #              date_labels = "%b %Y") +
-        #scale_x_continuous(breaks = NULL)+
-        theme_bw() + 
-        theme(panel.grid.major = element_blank(), 
-              panel.grid.minor = element_blank(), 
-              panel.background = element_blank(), 
-              axis.line = element_line(colour = "black"))+
-        theme(axis.text.x = element_text(angle = 45, hjust = 1),
-              plot.margin = margin(0.25, 0.45, 0.25, 0.25, "inch"))+
-        theme(strip.background = element_rect(fill = "white"))
-  })
-  
-  ## Plot 2
-  output$OverallTrendsPlot2 <- renderPlot({
-    dfAll %>%
-      filter(Date >= input$OveralldateRange[1] & Date <= input$OveralldateRange[2]) %>%
-      filter(`Time Interval` == input$OverallMedian2) %>%
+    dfAll_otp1 %>%
+      filter(between(Date,input$OverallDateRange[1], input$OverallDateRange[2])) %>%
+      filter(`Time Interval` == input$OverallMedian1) %>%
       mutate(Date = as.Date(Date)) %>%
-      ggplot(aes_string(x = "Date", 
-                        y = input$OverallVariable2)) +
+      ggplot(aes(x = Date, 
+                 y = input_var))+
       geom_line()+
       # geom_smooth() +
       # scale_x_date(date_breaks = "1 year",
       #              date_labels = "%b %Y") +
+      #scale_x_continuous(breaks = NULL)+
+      theme_bw() + 
+      theme(panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank(), 
+            panel.background = element_blank(), 
+            axis.line = element_line(colour = "black"))+
+      theme(axis.text.x = element_text(angle = 45, hjust = 1),
+            plot.margin = margin(0.25, 0.45, 0.25, 0.25, "inch"))+
+      theme(strip.background = element_rect(fill = "white"))
+    # }
+  })
+  
+  ## Plot 2
+  output$OverallTrendsPlot2 <- renderPlot({
+    dfAll_otp2 <- dfBaseData[, c("Time Interval", "Date", input$OverallVariable2)]
+    colnames(dfAll_otp2) <- c("Time Interval", "Date", "input_var")
+    
+    dfAll_otp2 %>%
+      filter(between(Date,input$OverallDateRange[1], input$OverallDateRange[2])) %>%
+      filter(`Time Interval` == input$OverallMedian2) %>%
+      mutate(Date = as.Date(Date)) %>%
+      ggplot(aes(x = Date, 
+                 y = input_var))+
+      geom_line()+
+      # geom_smooth() +
+      # scale_x_date(date_breaks = "1 year",
+      #              date_labels = "%b %Y") +
+      #scale_x_continuous(breaks = NULL)+
       theme_bw() + 
       theme(panel.grid.major = element_blank(), 
             panel.grid.minor = element_blank(), 
@@ -284,16 +360,20 @@ server <- function(input, output) {
   
   ## Plot 3
   output$OverallTrendsPlot3 <- renderPlot({
-    dfAll %>%
-      filter(Date >= input$OveralldateRange[1] & Date <= input$OveralldateRange[2]) %>%
+    dfAll_otp3 <- dfBaseData[, c("Time Interval", "Date", input$OverallVariable3)]
+    colnames(dfAll_otp3) <- c("Time Interval", "Date", "input_var")
+    
+    dfAll_otp3 %>%
+      filter(between(Date,input$OverallDateRange[1], input$OverallDateRange[2])) %>%
       filter(`Time Interval` == input$OverallMedian3) %>%
-      select(Date, input$OverallVariable3) %>%
-      ggplot(aes_string(x = "Date", 
-                        y = input$OverallVariable3)) +
+      mutate(Date = as.Date(Date)) %>%
+      ggplot(aes(x = Date, 
+                 y = input_var))+
       geom_line()+
       # geom_smooth() +
       # scale_x_date(date_breaks = "1 year",
       #              date_labels = "%b %Y") +
+      #scale_x_continuous(breaks = NULL)+
       theme_bw() + 
       theme(panel.grid.major = element_blank(), 
             panel.grid.minor = element_blank(), 
@@ -306,16 +386,20 @@ server <- function(input, output) {
   
   ## Plot 4
   output$OverallTrendsPlot4 <- renderPlot({
-    dfAll %>%
-      filter(Date >= input$OveralldateRange[1] & Date <= input$OveralldateRange[2]) %>%
+    dfAll_otp4 <- dfBaseData[, c("Time Interval", "Date", input$OverallVariable4)]
+    colnames(dfAll_otp4) <- c("Time Interval", "Date", "input_var")
+    
+    dfAll_otp4 %>%
+      filter(between(Date,input$OverallDateRange[1], input$OverallDateRange[2])) %>%
       filter(`Time Interval` == input$OverallMedian4) %>%
       mutate(Date = as.Date(Date)) %>%
-      ggplot(aes_string(x = "Date", 
-                        y = input$OverallVariable4)) +
+      ggplot(aes(x = Date, 
+                 y = input_var))+
       geom_line()+
       # geom_smooth() +
       # scale_x_date(date_breaks = "1 year",
       #              date_labels = "%b %Y") +
+      #scale_x_continuous(breaks = NULL)+
       theme_bw() + 
       theme(panel.grid.major = element_blank(), 
             panel.grid.minor = element_blank(), 

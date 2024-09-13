7e72fb4b3d0cf6b6edf13c188e6ece0eb5daf3e3
rgrambihler <rgrambihler@email.arizona.edu>
2024-09-12 21:15:17 -0700

    finished about

--- RShiny/B2OWaterQualityApp.R
+++ RShiny/B2OWaterQualityApp.R
@@ -28,8 +28,10 @@ options(scipen = 999)
 # all Hach and YSI data: combo of all above files with "Time Interval" column to 
 # denote which interval (daily, weekly, etc) the data comes from
 dfAll <- read_csv("SourceData/05-HachYSI-Data-All.csv", col_names = TRUE) %>%
-  select(-`pH_Hach`) %>%
-  rename(pH = `pH (YSI)`)
+  select(-`pH_Hach`, -`Conductivity, Non-Linear Function (µS/cm)`, -`Conductivity, Specific (µS/cm)`,
+         -`Dissolved Oxygen, Saturated (%)`, -`pH (mV)`, ) %>%
+  rename(pH = `pH (YSI)`) %>%
+  rename(`Turbidity (FAU)` = `Turbidity (RFU)`)
 
 # World Oceans Data for B1 Comparisons
 dfWOD <- read_csv("SourceData/02-Clean-WorldOceanData-Final.csv")%>%
@@ -44,8 +46,7 @@ dfWOD <- read_csv("SourceData/02-Clean-WorldOceanData-Final.csv")%>%
 # create nested list of correlation matrices: p-values, n, r values
 cor_mat_stats <- dfAll %>%
   filter(`Time Interval` == "Daily") %>%
-  select(-`Time Interval`, -Date, -`pH (mV)`, -`Conductivity, Non-Linear Function (µS/cm)`,
-         -`Conductivity, Specific (µS/cm)`, -`Dissolved Oxygen, Saturated (%)`) 
+  select(-`Time Interval`, -Date) 
 cor_mat_stats <- rcorr(as.matrix(cor_mat_stats))
 # full flattened correlation matrix: table of correlation pairings using matrices created in cor_mat_stats
 cor_mat_stats_flat <- flattenCorrMatrix(cor_mat_stats$r, cor_mat_stats$P) %>%
@@ -119,17 +120,13 @@ var_choices <- c("None",
                  "Alkalinity (mg/L CaCO3)", 
                  "Chlorophyll (RFU)",
                  "Conductivity (µS/cm)",
-                 "Conductivity, Non-Linear Function (µS/cm)",
-                 "Conductivity, Specific (µS/cm)",
                  "Dissolved Oxygen (mg/L)",
                  "Dissolved Oxygen, Local (%)",
-                 "Dissolved Oxygen, Saturated (%)",
                  "Iron (mg/L)",
                  "Nitrate, Mid-Range (mg/L)", 
                  "Nitrate, High-Range (mg/L)",
                  "Oxygen Reduction Potential (mV)",
                  "pH",
-                 "pH (mV)",
                  "Phosphate (mg/L)",
                  "Salinity (PSU)",
                  "Silica (mg/L)",
@@ -137,7 +134,7 @@ var_choices <- c("None",
                  "Temperature (ºF)",
                  "Total Algae (RFU)",
                  "Total Dissolved Solids (mg/L)",
-                 "Turbidity (RFU)")
+                 "Turbidity (FAU)")
 
 # character vector of UI choices for `Time Interval`
 median_period_choices <- c("Daily", "Weekly", "Monthly", "Yearly")
@@ -148,7 +145,7 @@ median_period_choices <- c("Daily", "Weekly", "Monthly", "Yearly")
 ui <- page_fillable(
   # title panel
   titlePanel(title = div(imageOutput("B2Header", inline = TRUE), 
-                         "Ocean Water Quality", 
+                         "Ocean Water Quality Dashboard", 
                          style = "color: #49595e;"),
              windowTitle = "Biosphere2OceanWaterQuality"),
   # page with navigation bar at top to click to other pages (panels)
@@ -156,15 +153,86 @@ ui <- page_fillable(
               inverse = FALSE,
               ##### About Page #####
               nav_panel(title = "About",
-                        layout_column_wrap(
-                          width = 1/2,
-                          gap = "50px",
-                          div(imageOutput(outputId = "B2OImage"),
-                              tags$figcaption("Biosphere 2 Ocean: 700,000-gallon Experimental Mesocosm",
-                                              style = "color: #C0C0C0;")
-                              ),
-                          textOutput("AboutText")
-                        )
+                        fluidPage(
+                          fluidRow(
+                            column(
+                              width = 7,
+                              imageOutput("B2OImage")
+                            ),
+                            column(
+                              width = 5,
+                              p("The visualizations presented on this site are made from water quality parameter data collected from a combination of sources over the course of the Biosphere 2 Ocean (B2O) project."),
+                              br(),
+                              p("For questions about this site and/or the data used here, please contact Renee Grambihler: grambihler@arizona.edu")
+                            )
+                          ),
+                       accordion(
+                         open = FALSE,
+                         accordion_panel(
+                           "Metadata",
+                           h6("Variable Meanings and Units"),
+                           p("Ammonia (mg/L): amount of Ammonia (NH3) in range 0.01-0.50 measured in mg/L"),
+                           p("Alkalinity (mg/L CaCO3): amount of Calcium Carbonate (CaCO3) in range 100-400 measured in mg/L"), 
+                           p("Chlorophyll (RFU): amount of Chlorophyll measured in Relative Fluorescence Units"),
+                           p("Conductivity (µS/cm): measures Electrical Conductance in micro-Semens/cm; salinity measurements are calculated from this value"),
+                           p("Dissolved Oxygen (mg/L): amount of Oxygen dissolved in seawater measured in mg/L"),
+                           p("Dissolved Oxygen, Local (%): amount of Oxygen dissolved in seawater measured by percent"),
+                           p("Iron (mg/L): amount of Iron in range 0.009-1.400 measured in mg/L"),
+                           p("Nitrate, Mid-Range (mg/L): amount of Nitrate (NO3-) from 0.2-5.0 measured in mg/L"), 
+                           p("Nitrate, High-Range (mg/L): amount of Nitrate (NO3-) from 0.3-30.0 measured in mg/L"),
+                           p("Oxygen Reduction Potential (mV): measures ability of water to gain or lose electrons measured in miliVolts; used as indication of amount of oxygen in the water"),
+                           p("pH: pH"),
+                           p("Phosphate (mg/L): amount of Phosphate (PO4 3-) in range 0.02-2.50 measured in mg/L"),
+                           p("Salinity (PSU): amount of salt measured in Practical Salinity Units; equivalent to PPT"),
+                           p("Silica (mg/L): amount of Silica (SiO2) in range 0.01-1.60 measured in mg/L"),
+                           p("Temperature (ºC): temperature measured in Celcius"),
+                           p("Temperature (ºF): temperature measured in Fahrenheit"),
+                           p("Total Algae (RFU): amount of Phycoerytherin algae measured in Relative Fluorescence Units"),
+                           p("Total Dissolved Solids (mg/L): amount of solids dissolved in the water measured in mg/L"),
+                           p("Turbidity (FAU): measures water transparency in the range 21-1000 Formazine Attenuation Units")
+                         ),
+                         accordion_panel(
+                           "Data Sources",
+                           h6("Biosphere 2 Ocean"),
+                           p("Data starts in 2011 with Temperature (ºC), Salinity (ppt), Conductivity (mS/cm), Oxygen Reduction Potential (mV) (abbreviated as ORP), and pH."),
+                           p("In 2014, a YSI 6600 V2 multiparameter sonde was installed in the B2O and set to record Temperature (ºF and ºC), pH, Salinity (ppt), Conductivity (mS/cm), ORP (mV), Phycoerythrin (RFU), Chlorophyll (µg/L), and Dissolved Oxygen (%)."),
+                           p("In 2018, the ocean team started collecting weekly nutrient samples of Turbidity (FAU), Phosphate (mg/L), Nitrate (mg/L), Alkalinity (mg/L), Silicate (mg/L), and Iron (mg/L). This is done via colorimetry and digital titration (Hach brand)."),
+                           p("In 2021, a new YSI EXO3 sonde was installed in the B2O and set to record Temperature (ºF and ºC), pH, Salinity (PSU), Conductivity (µS/cm), Dissolved Oxygen (mg/L and %), ORP (mV), Chlorophyll (RFU), Total Algae (RFU), and Total Dissolved Solids (RFU) at 15-minute intervals."),
+                           h6("Biosphere 1 Oceans"),
+                           p("Data from Biosphere 1 oceans (Atlantic, Pacific, etc) were downloaded and compiled from NOAA databases (see References for citation).")
+                         ),
+                         accordion_panel(
+                           "Quality Control",
+                           h6("Overall"),
+                           p("Data was run through a function that identifies outlier data points beyond 2 standard deviations from the monthly mean or median for each variable. The use of mean or median was decided by running a Shapiro-Wilk Test for Normality on the data for each month. Normal data was compared to the mean. Non-normal data was compared to the median. Outliers beyond 2 standard deviations were turned into NA values."),
+                           h6("For this website"),
+                           p("Data used in this website is from the overall quality control process. However, to increase the usefulness of visualizations, medians of the data were taken on daily, weekly, monthly, and yearly intervals. The smallest unit of data presented here is a median of data taken in a 24-hour period. Median was used because the original quality control revealed that most of the data tends to be non-normal, though the size of this dataset does usually give the same mean and median value for at least monthly and yearly intervals.")
+                         ),
+                         accordion_panel(
+                           "Historical Context",
+                           p("While most of the data follows the same seasonal trends that are exhibited by B1 oceans, there are some discrepancies that can be explained through the history of the B2O."),
+                           h6("1996-2013:"),
+                           p("The B2O was largely un-cared for and unmonitored until 2013. The Biosphere 2 facility was acquired by the University of Arizona in July 2011, which is where our dataset starts, but its validity from 2011-2013 cannot be 100% confirmed."),
+                           h6("2013-2018:"),
+                           p("Dr. Rafe Sagarin became employed as the Ocean Program Director and decided to model the B2O after the Sea of Cortez due to its proximity, meaning that seasonal cycles would be similar in both systems. This allowed for a detailed education program about the Sea of Cortez and its importance for the weather patterns of Tucson."),
+                           p("In 2014, Dr. Sagarin introduced a YSI 6600 V2 Multiparameter Sonde to the B2O, which increased the number of parameters and the validity of the data recorded."),
+                           p("In 2015, Dr. Julia Cole succeeded Dr. Sagarin as Ocean Research Director. With a background in coral research (specifically in using coral skeletons to reconstruct past climate conditions), Dr. Cole decided the new purpose of the B2O would be to replicate conditions found in tropical oceans and use the system as an experimental mesocosm for testing the validity of radical solutions for coral reef restoration both in current and future projected climactic conditions."),
+                           p("The increase in salinity seen from 2015-2018 is due to staff mixing artificial saltwater and adding it to the system to better replicate the conditions seen on coral reefs in tropical oceans worldwide."),
+                           h6("2018-Present:"),
+                           p("In 2018, Dr. Cole left her position as Ocean Research Director and was succeeded by fellow coral paleogeologist, Dr. Diane Thompson, who still currently holds this position. Dr. Thompson has continued the purpose of the B2O as an experimental mesocosm dedicated to testing solutions for coral reef restoration that are not yet currently able to be deployed in B1 Oceans."),
+                           p("During this time, Dr. Thompson has overseen the installation of a temperature regulator that can change the temperature of all 700,000-gallons of water by 2ºC in 24 hours. This is the reason why the temperature data flattens out in 2019. She also installed a new YSI EXO3 Multiparameter Sonde to replace the old one."),
+                           p("From 2018-2020, the B2O team pulled out 1000s of pounds of algae from the reef, which can be seen throughout the data."),
+                           p("2020-2021 brought with it the COVID-19 pandemic and a turnover in staff, which left the B2O understaffed, a possible contributor to any major and/or sudden shifts and gaps in the data."),
+                           p("Staffing has been stable since 2022 with the hiring of Research Specialists Lia Crocker and Renee Grambihler who have backgrounds in engineering and bioinformatics, respectively.")
+                         ),
+                         accordion_panel(
+                           "References",
+                           p("National Oceanic and Atmospheric Administration; NOAA Atlantic Oceanographic and Meteorological Laboratory; Cooperative Institute for Marine and Atmospheric Studies (2018). National Coral Reef Monitoring Program: Carbonate chemistry data collected in the Atlantic Ocean. [indicate subset used]. NOAA National Centers for Environmental Information. Dataset. https://doi.org/10.25921/vfz0-dg77. Accessed Sept 9 2024. "),
+                           p("Ecosystem Sciences Division, Pacific Islands Fisheries Science Center (2018). National Coral Reef Monitoring Program: Water chemistry of the coral reefs in the Pacific Ocean. [indicate subset used]. NOAA National Centers for Environmental Information. Dataset. https://doi.org/10.25921/nnk7-5f86. Accessed Sept 9 2024. "),
+                           p("NCEI World Ocean Database ")
+                         )
+                       )
+              )
               ),
               
               ##### Time Series Page #####
@@ -469,17 +537,13 @@ server <- function(input, output) {
   # header image
     output$B2Header <- renderImage({
       list(src = "SourceImages/Webheader-Biosphere_0_0.png",
-           style = 'height: 50%; width: 25%')
+           style = 'height: 25%; width: 200px')
     }, deleteFile = FALSE)
   ##### About #####
     # b2o image
     output$B2OImage <- renderImage({
-      list(src = "SourceImages/B2O-image.png", style='width: 600px; height: 400px')
+      list(src = "SourceImages/B2O-image.png", style='width: 600px; height: 350px')
     }, deleteFile = FALSE)
-    # body text
-    output$AboutText <- renderText({
-      "text here"
-    })
   ##### Time Series #####
     ##### Overall Trends #####
     # Plot 1
